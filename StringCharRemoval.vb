c2a1ca649b6cb06850fd5c486d9e0726e4e4e056
Rohan2k2 <55294717+Rohan2k2@users.noreply.github.com>
2019-10-03 20:50:33 +0500

    Character removal in a string input
    in the context of a phone number entering program

---
+++ StringCharRemoval.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim NumEntered, FixedNum, OmittedChar, CurrentChar As String
+        Dim counter As Integer
+
+        NumEntered = ""
+        FixedNum = ""
+        OmittedChar = ""
+        CurrentChar = ""
+        counter = 0
+
+        Console.Write("Enter your phone number: ")
+        NumEntered = Console.ReadLine
+
+        Console.Write("Enter the character to remove: ")
+        OmittedChar = Console.ReadLine
+
+        For counter = 1 To Len(NumEntered)
+            CurrentChar = Mid(NumEntered, counter, 1)
+            If CurrentChar <> OmittedChar Then
+                FixedNum = FixedNum & CurrentChar
+            End If
+        Next
+        Console.WriteLine("Your phone number without any extra characters is: " & FixedNum)
+        Console.ReadKey()
+    End Sub
+
+End Module
