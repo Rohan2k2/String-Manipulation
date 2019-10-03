be29a20b477b45104e616db3ed4d8f3ce7842c3b
Rohan2k2 <55294717+Rohan2k2@users.noreply.github.com>
2019-10-03 20:52:24 +0500

    Replacing a certain character in a string input
    takes place in the context of a phone number entering program,  includes addition of country code

---
+++ StringCharReplace.vb
@@ -0,0 +1,43 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim NumEntered, InternationalNum, OmittedChar, ReplacedChar, CountryCode, FixedInternationalNum, CurrentChar As String
+        Dim counter As Integer
+
+        NumEntered = ""
+        InternationalNum = ""
+        FixedInternationalNum = ""
+        OmittedChar = ""
+        CurrentChar = ""
+        ReplacedChar = ""
+        CountryCode = ""
+        counter = 0
+
+        Console.Write("Enter your phone number: ")
+        NumEntered = Console.ReadLine
+
+        Console.Write("Entered the characters to be omitted in the string: ")
+        OmittedChar = Console.ReadLine
+
+        Console.Write("Enter the character to replace the omitted character: ")
+        ReplacedChar = Console.ReadLine
+
+        Console.Write("Enter your country code: ")
+        CountryCode = Console.ReadLine
+
+        InternationalNum = CountryCode & Right(NumEntered, Len(NumEntered) - InStr(NumEntered, "0"))
+        For counter = 1 To Len(InternationalNum)
+            CurrentChar = Mid(InternationalNum, counter, 1)
+            If CurrentChar = OmittedChar Then
+                CurrentChar = ReplacedChar
+            End If
+            FixedInternationalNum = FixedInternationalNum & CurrentChar
+        Next
+
+        Console.WriteLine("Your phone number with the country code and with the characters you requested to be replaced is: " & FixedInternationalNum)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
