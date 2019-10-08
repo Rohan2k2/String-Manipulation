e6a299a1321d18527911f3c1e589eb3dea2febfc
Rohan2k2 <55294717+Rohan2k2@users.noreply.github.com>
2019-10-08 16:02:36 +0500

    counts the amount of vowels in an entered string

---
+++ VovelCounter.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str, vovel As String
+        Dim count, NoOfVovels As Integer
+
+        str = ""
+        vovel = "AEIOU"
+        count = 0
+        NoOfVovels = 0
+
+        Console.Write("Enter the string to be checked: ")
+        str = Console.ReadLine
+
+        str = UCase(str)
+
+        For count = 1 To Len(str)
+            If InStr(vovel, Mid(str, count, 1)) > 0 Then
+                NoOfVovels = NoOfVovels + 1
+            End If
+        Next
+
+        Console.WriteLine("the no of vovels in the entered string is: " & NoOfVovels)
+        Console.ReadKey()
+    End Sub
+
+End Module
