137461e7e6bd17ab2c76e5dc78b760fd9725d77a
Rohan2k2 <55294717+Rohan2k2@users.noreply.github.com>
2019-10-15 10:45:42 +0500

    Combining the first word of a string with the second word of a different string

---
+++ StringConcatenation.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, str2, str3 As String
+
+        str1 = ""
+        str2 = ""
+        str3 = ""
+
+        Console.Write("enter first string of 2 words: ")
+        str1 = Console.ReadLine
+
+        Console.Write("enter second string of 2 words: ")
+        str2 = Console.ReadLine
+
+        str3 = Left(str1, InStr(str1, " ")) & Right(str2, Len(str2) - InStr(str2, " "))
+
+        Console.WriteLine("the combined string is: " & str3)
+        Console.ReadKey()
+    End Sub
+
+End Module
